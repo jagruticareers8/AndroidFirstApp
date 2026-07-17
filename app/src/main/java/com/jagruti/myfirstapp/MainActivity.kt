9fb19c9f9f2e3963edc5f8daaf17c6c93ced3ff9
jagruti <jagruticareers8@gmail.com>
2026-07-17 11:31:18 +0530

    Add RealTime Data base for User

--- app/src/main/java/com/jagruti/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/jagruti/myfirstapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.jagruti.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val etMail = findViewById<EditText>(R.id.etMail)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etUserName = findViewById<EditText>(R.id.etUserName)
+
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val mail =etMail.text.toString()
+            val uid= etUserName.text.toString()
+            val  pwd = etPassword.text.toString()
+
+            val  user = User(name,mail,pwd,uid)
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child(uid).setValue(user).addOnSuccessListener {
+                etName.text.clear()
+                etMail.text.clear()
+                etPassword.text.clear()
+                etUserName.text.clear()
+
+                Toast.makeText(this, "User Registered", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show()
+
+            }
         }
     }
 }
---
+++ app/src/main/java/com/jagruti/myfirstapp/User.kt
@@ -0,0 +1,5 @@
+package com.jagruti.myfirstapp
+
+data class User (val name : String, val  email : String, val password : String, val uId : String){
+
+}
